1fa29745ce9518fb3e8e52f046b711fc1c8c726d
ewasawczynska <sawczynska.e@gmail.com>
2024-02-15 21:49:26 +0100

    work preparation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './sass/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RegisterPage } from './pages/RegisterPage/RegisterPage';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RegisterPage />
+  </React.StrictMode>
+);
---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+export class RegisterPage extends Component {
+  
+    render() {
+      return (
+        <p> test </p>
+      );
+    };
+};
